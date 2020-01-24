d1180799e988ec4184801e33549991a7391ce770
Jieun Kang <jieunkang101@gmail.com>
2020-01-24 14:20:01 -0800

    try to find out make working syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-// business logic
+// =============business logic=============
 function output(input) {
   if (input >=0) {
-    
     return rangeOfNumber(input);
   } else {
     alert("Error: Please, enter a Positive Number");
@@ -11,34 +10,30 @@ function output(input) {
 
 function rangeOfNumber(input) {
   var numArr = Array.from(Array(input +1).keys());
+  //var numArr = [];
+  for (var i=0; i <= numArr.length; i++) {
+    if (numArr.indexOf(3)) {
+      var numContain3 = numArr[numArr.indexOf(3)].toString();
+      numContain3.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.")
+  }
+ 
+      //return numArr.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.");
+      
+
+      // var numContain3 = numArr[numArrStr.indexOf(3)].toString();
+      // numContain3.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.");
+      // return numArrStr;
   return numArr;
+}
 }
 
 
-    //var numArrStr = numArr.toString().split(",")
-    //for (var i=0; i < numArrStr.length; i++)
-    
-//     if (numArr.includes(3)) {
-//       var numContain3 = numArr[numArrStr.indexOf("3")].toString();
-//       numContain3.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.");
-//       return numArrStr;
-//     }
 
-
- 
-
-
-
-// user interface logic
+// =========user interface logic===========
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#number").val());
-    
- 
-  
-
-    var result = output(input);
-    $("#output").text(result);    
+    var input = parseInt($("input#userInput").val());
+    $("#output").text(output(input));
   });
 });
