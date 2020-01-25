16b5977a26b5aa5557f19255e992c625113897ee
Jieun Kang <jieunkang101@gmail.com>
2020-01-24 16:12:00 -0800

    make conditions for numTwo and numOne

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,45 @@
 // =============business logic=============
 function output(input) {
   if (input >=0) {
-    return rangeOfNumber(input);
+    return replace(input);
   } else {
     alert("Error: Please, enter a Positive Number");
     location.reload();
   }
 }
 
-function rangeOfNumber(input){
+function replace(input) {
+  //var numArr = Array.from(Array(input +1).keys());
   var numArr = [];
   for (var i=0; i <= input; i++) {
-    numArr.push(i);
-    if (numArr.includes(3)) {
-      var numThree = numArr[numArr.indexOf(3)].toString();
-      var output = numThree.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.")
+    numArr.push(i);}
+  if (numArr.includes(3)) {
+    var numThree = numArr[numArr.indexOf(3)].toString();
+    var output = numThree.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.")
+    return output;
+    } else if (numArr.includes(2)) {
+      var numTwo = numArr[numArr.indexOf(2)].toString();
+      var output = numTwo.replace("2", "Boop!")
       return output;
-      }
-  }
-  
-
-
-
-
- // return numArr;
+      } else if (numArr.includes(1)) {
+        var numOne = numArr[numArr.indexOf(1)].toString();
+        var output = numOne.replace("1", "Beep!")
+        return output;
+        } 
+  return numArr;
 }
 
 
-
-
-
+// function rangeOfNumber(input){
+//   var numArr = [];
+//   for (var i=0; i <= input; i++) {
+//     numArr.push(i);}
+//     return numArr; 
+// }    
 
 
   // var numArr = Array.from(Array(input +1).keys());
-  //for (var i=0; i <= numArr.length; i++) {
-  //   
- 
-      //return numArr.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.");
       
-
-      // var numContain3 = numArr[numArrStr.indexOf(3)].toString();
-      // numContain3.replace("3", "I'm sorry, Dave. I'm afraid I can't do that.");
-      // return numArrStr;
-//   return numArr;
-// }
-
-
-
 
 // =========user interface logic===========
 $(document).ready(function() {
